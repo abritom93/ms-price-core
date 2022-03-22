3f7ab940f04ffb34d256070955a3a139ee27c31a
Ariel Brito <abritom93@gmail.com>
2022-03-22 10:18:24 -0300

    Implementing unit test

---
+++ src/test/java/cl/price/core/ApplicationTest.java
@@ -0,0 +1,30 @@
+package cl.price.core;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import cl.price.core.adapters.controllers.PriceController;
+import cl.price.core.adapters.jdbc.PriceJpaAdapter;
+import cl.price.core.application.usecases.GetPriceUseCase;
+
+@SpringBootTest
+public class ApplicationTest {
+
+	@Autowired
+	private PriceController priceController;
+	
+	@Autowired
+	private GetPriceUseCase getProductPriceUseCase;
+	
+	@Autowired
+	private PriceJpaAdapter priceJdbcAdapter;
+	
+	@Test
+	public void loadContext(){
+		Application.main(new String[] {});
+		assertNotNull(priceController);
+		assertNotNull(getProductPriceUseCase);
+		assertNotNull(priceJdbcAdapter);
+	}
+}
